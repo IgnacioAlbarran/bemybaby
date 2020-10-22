bcccb53620a3619f21d6bc1548d4c06e3cea55e5
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2020-10-22 12:48:13 +0200

    codestyling

--- spec/models/baby_spec.rb
+++ spec/models/baby_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Baby, type: :model do
-  let!(:father) { create(:user, name: 'Pepito', last_name: 'Piscinas', email: 'pepito@piscinas.com') }
-  let!(:son) { create(:baby, user_id: father.id, name: 'Pepito', last_name: 'Piscinas Jr') }
+  let!(:father) do
+    create(:user, name: 'Pepito', last_name: 'Piscinas',
+                  email: 'pepito@piscinas.com')
+  end
+
+  let!(:son) do
+    create(:baby, user_id: father.id, name: 'Pepito', last_name: 'Piscinas Jr')
+  end
 
   it { should belong_to(:user) }
 
@@ -24,8 +30,9 @@ RSpec.describe Baby, type: :model do
 
   it 'a baby with NO same name, last_name, dob and blood_type is valid' do
     new_baby_attributes = {
-      name: 'Jaimito', last_name: 'El picaro', dob: '2018-01-01', gender: 'niño',
-      blood_type: 'A-', user_id: father.id, created_at: Time.now, updated_at: Time.now
+      name: 'Jaimito', last_name: 'El picaro', dob: '2018-01-01',
+      gender: 'niño', blood_type: 'A-', user_id: father.id,
+      created_at: Time.now, updated_at: Time.now
     }
 
     expect(Baby.new(new_baby_attributes)).to be_valid
@@ -33,8 +40,9 @@ RSpec.describe Baby, type: :model do
 
   it 'a baby with SAME name, last_name, dob and blood_type is invalid' do
     existing_baby_attributes = {
-      name: 'Pepito', last_name: 'Piscinas Jr', dob: '2018-01-01', gender: 'niño',
-      blood_type: 'A-', user_id: father.id, created_at: Time.now, updated_at: Time.now
+      name: 'Pepito', last_name: 'Piscinas Jr', dob: '2018-01-01',
+      gender: 'niño', blood_type: 'A-', user_id: father.id,
+      created_at: Time.now, updated_at: Time.now
     }
 
     expect(Baby.new(existing_baby_attributes)).not_to be_valid
