4043c9fa68455c100794a3a06f103b4921615dba
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2021-09-06 14:37:05 +0200

    mailer running already!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,9 +8,14 @@ class UserMailer < ApplicationMailer
   def welcome_email(email)
     # @user = params[:user]
     # @url  = user_url(@user)
-     delivery_options = { user_name: 'baby book',
-                         password: 'perico132435',
-                         address: 'lperico127@gmail.com' }
+    delivery_options = { user_name: ENV["GMAIL_USER_NAME"],
+                         password: ENV["GMAIL_PASSWORD"],
+                         address: "smtp.gmail.com",
+                         port: 587,
+                         domain: "smtp.gmail.com",
+                         authentication: "plain",
+                         enable_starttls_auto: true,
+                         openssl_verify_mode: "none" }
     mail(to: email,
          subject: "Please see the Terms and Conditions attached",
          delivery_method_options: delivery_options)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,18 +35,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'http://localhost' #replace with your own url
+  host = 'localhost' #replace with your own url
   config.action_mailer.default_url_options = { host: host, port: 3000 }
 
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => "lperico127@gmail.com",
-    :password             => "perico132435",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {}
 
   config.action_mailer.raise_delivery_errors = true
 
